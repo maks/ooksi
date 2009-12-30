9530ef20f5cf6f686e88d6ea010dcb6c561064d4
maks <maks@manichord.com>
2009-12-31 08:43:14 +1100

    switched back to using MIDlet subclass

---
+++ src/com/ooksi/OoksiMIDlet.java
@@ -0,0 +1,11 @@
+package com.ooksi;
+
+import javax.microedition.midlet.MIDlet;
+
+import org.kobjects.kui.KItem;
+
+public abstract class OoksiMIDlet extends MIDlet {
+	
+	public abstract Object initStartDisplay();
+
+}
